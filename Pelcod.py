82fe007d7f3e74efc21a49f1a38d2d8c972a261a
Desi <deskaidra@gmail.com>
2026-09-20 08:45:43 +0700

    Desi pelcod

---
+++ Pelcod.py
@@ -0,0 +1,42 @@
+nama_1 = "Desi Kamla Indriana"
+
+print(nama_1)
+
+angka1 = 11
+angka2 = 5
+print(angka1 % angka2)
+
+a = 2
+b = 2
+print(a ** 2)
+
+print("== And ==")
+x = 5
+print(x > 2 and x < 10)
+
+print("== Or ==")
+x = 5
+print(x < 2 or x < 8 )
+
+print("== Not ==")
+x = 5
+print(not(x > 2 and x < 10))
+
+print("== if-elif-else")
+angka = 0
+if angka > 0:
+    print("angka positif")
+elif angka < 0:
+    print("angka negatif")
+else:
+    print("angka nol")
+
+a = 5
+if a > 10:
+    print("A")
+elif a == 0:
+    print("B")
+elif a < 8:
+    print("C")
+else:
+    print("D")
