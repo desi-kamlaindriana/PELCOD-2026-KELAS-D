b15aa917851ab26c478c0fc36abf16c4b819b34a
Desi <deskaidra@gmail.com>
2026-09-20 10:11:53 +0700

    Desi1

---
+++ desilatihan.py
@@ -0,0 +1,18 @@
+nama_peminjam = input()
+no_anggota = 1
+jumlah_buku = 2
+status = True
+durasi = 8
+
+print(nama_peminjam)
+
+jumlah_tiket = "4"
+harga_tiket =  25000
+
+total_sementara = jumlah_tiket*2
+print("Total sementara:", total_sementara, "Harga tiket:", harga_tiket)
+
+# jumlah tiket
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket * harga_tiket
+print("Total akhir.", total_akhir)
